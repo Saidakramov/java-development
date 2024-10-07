441e9eeb01071bcec01d6aeefb191e63151b9500
Akbar <akbarsaidakramov@gmail.com>
2024-10-07 15:48:46 -0700

    adding loop exercises

---
+++ workbook-2/LoopingExercises/src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int x= 0;
+
+        do {
+            System.out.println("I love Java!");
+            x++;
+        } while (x < 5); {
+
+        }
+    }
+}
